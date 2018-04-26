f7db5b0399ad55150f4cb16d5b595be580f3e780
Spyros Argyroiliopoulos <spyros.argyroiliopoulos@camelotglobal.com>
2018-04-27 02:00:48 +0300

    Initial commit of new project

---
+++ src/main/java/com/codehub/spring/SpringCoreCodeExamples/SpringCoreCodeExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.codehub.spring.SpringCoreCodeExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreCodeExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreCodeExamplesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codehub/spring/SpringCoreCodeExamples/SpringCoreCodeExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codehub.spring.SpringCoreCodeExamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCoreCodeExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
