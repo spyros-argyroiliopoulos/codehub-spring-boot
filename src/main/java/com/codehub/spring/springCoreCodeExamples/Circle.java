0006b52f138203d9ebe19ca3b3095d83ce2eef28
Spyros Argyroiliopoulos <spyros.argyroiliopoulos@camelotglobal.com>
2018-04-27 03:35:07 +0300

    Before

---
+++ src/main/java/com/codehub/spring/springCoreCodeExamples/Circle.java
@@ -0,0 +1,13 @@
+package com.codehub.spring.springCoreCodeExamples;
+
+public class Circle {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
