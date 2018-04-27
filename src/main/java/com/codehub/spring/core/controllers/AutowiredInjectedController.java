1e7805efc64b3c7892bd724244fe098a3902366e
Spyros Argyroiliopoulos <spyros.argyroiliopoulos@camelotglobal.com>
2018-04-27 21:07:38 +0300

    Minor clean-up

--- src/main/java/com/codehub/spring/core/controllers/AutowiredInjectedController.java
+++ src/main/java/com/codehub/spring/core/controllers/AutowiredInjectedController.java
@@ -10,11 +10,14 @@ public class AutowiredInjectedController {
         return greetingService.sayGreeting();
     }
 
+    //To Autowire byType or byName I need to have at my class a public setter method
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
         System.out.println("Using setter too..");
     }
 
+    //To Autowire by Constructor I need to have at my class the appropriate public constructor
+    //Autowire by constructor will be preferred by Spring if both setter and constructor are present
 //    public AutowiredInjectedController(GreetingService greetingService) {
 //        this.greetingService = greetingService;
 //        System.out.println("..Or using constructor");
