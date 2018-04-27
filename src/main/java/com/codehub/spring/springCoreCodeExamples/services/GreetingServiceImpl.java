d60ddad26e407096bd30bee2e6f4c068aa85e2a6
Spyros Argyroiliopoulos <spyros.argyroiliopoulos@camelotglobal.com>
2018-04-27 19:24:29 +0300

    Qualifier solution

--- src/main/java/com/codehub/spring/springCoreCodeExamples/services/GreetingServiceImpl.java
+++ src/main/java/com/codehub/spring/springCoreCodeExamples/services/GreetingServiceImpl.java
@@ -1,9 +1,11 @@
 package com.codehub.spring.springCoreCodeExamples.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 //This annotation is needed to tell spring that this is a bean
 @Service
+@Primary
 public class GreetingServiceImpl implements GreetingService {
 
     public static final String HELLO_CODE_HUB = "Hello Code.Hub!";
