6f52796d38b1dce5153157be48ec0e450efd6e81
Spyros Argyroiliopoulos <spyros.argyroiliopoulos@camelotglobal.com>
2018-04-27 10:22:20 +0300

    Spring XML

--- src/main/java/com/codehub/spring/SpringCoreCodeExamples/SpringCoreCodeExamplesApplication.java
+++ src/main/java/com/codehub/spring/SpringCoreCodeExamples/SpringCoreCodeExamplesApplication.java
@@ -2,8 +2,10 @@ package com.codehub.spring.springCoreCodeExamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:app-config.xml")
 public class SpringCoreCodeExamplesApplication {
 
     public static void main(String[] args) {
